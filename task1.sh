38f4dd0a55ecdac0b9b51f703a05bce3527e6f45
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-10-31 17:41:52 +0600

    Added file that returns chessboard

---
+++ task1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~ 
+mkdir projectstemp
+cd projectstemp
+mkdir my_project
+cd my_project
+mkdir directory1 directory2 directory 3
+mkdir directory1/subdirectory1 directory3/subdirectory2
