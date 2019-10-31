7f8ef864d21f39185b4f19a854c0cd1a0de4b0dc
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-10-31 17:43:48 +0600

    Added sh file that creates repositories

--- task1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~ 
-mkdir projectstemp
-cd projectstemp
-mkdir my_project
-cd my_project
-mkdir directory1 directory2 directory 3
-mkdir directory1/subdirectory1 directory3/subdirectory2
